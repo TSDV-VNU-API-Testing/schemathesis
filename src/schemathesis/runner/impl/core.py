4e774a17bd0be246fb72c86830fa9ac9ac7f971d
cheesedz <chisnghia@gmail.com>
2024-07-21 16:25:24 +0700

    fix: inconsistency in content length of the old and new image

--- src/schemathesis/runner/impl/core.py
+++ src/schemathesis/runner/impl/core.py
@@ -791,26 +791,15 @@ def _network_test(
     run_targets(targets, context)
     status = Status.success
     try:
-        logger.debug("kwargs in core.py: %s", kwargs)
+        #logger.debug("kwargs in core.py: %s", kwargs)
         requests_kwargs = case.as_requests_kwargs(
             base_url=case.get_full_base_url(), headers=headers
         )
         if 'files' in requests_kwargs:
             existing_tuple = requests_kwargs['files'][0]
-            logger.debug("existing tuple in kwargs in core.py: %s", existing_tuple)
+            #logger.debug("existing tuple in kwargs in core.py: %s", existing_tuple)
 
             if case.metadata is not None:
-                logger.debug("metadata in core.py: %s", case.metadata)
-                # for key, value in case.metadata.items():
-                #     logger.debug("key in metadata in core.py: %s", key)
-                #     if isinstance(case.body, dict) and key in case.body:
-                #         case.body[key] = case.metadata[key]["image_name"]
-
-                #         # Create a new tuple with the desired change
-                #         new_tuple = (existing_tuple[0], case.metadata[key]["image_name"])
-
-                #         # Update the list with the new tuple
-                #         requests_kwargs['files'][0] = new_tuple
                 for i, (key, value) in enumerate(requests_kwargs['files']):
                     # Check if the key is in metadata
                     if key in case.metadata:
@@ -823,14 +812,18 @@ def _network_test(
                         else:
                             # If not a tuple, directly update with the new image name
                             requests_kwargs['files'][i] = (key, case.metadata[key]["image_name"])
-                        logger.debug("Updated file in kwargs: %s", requests_kwargs['files'][i])
+                        #logger.debug("Updated file in kwargs: %s", requests_kwargs['files'][i])
         # requests_kwargs['files'][0][1] = case.metadata['image_name']
-            logger.debug("files in kwargs in core.py: %s", requests_kwargs['files'][0][1])
-        logger.debug("request_kargs in core.py: %s", requests_kwargs)
+            #logger.debug("files in kwargs in core.py: %s", requests_kwargs['files'][0][1])
+        #logger.debug("request_kargs in core.py: %s", requests_kwargs)
+        old_content_length = response.request.headers.get('Content-Length')
+        #logger.debug("old request content length in core.py: %s", old_content_length)
         request = requests.Request(**requests_kwargs).prepare()
-        logger.debug("request in core.py: %s", request.body)
+        #logger.debug("request content length in core.py: %s", request.headers.get('Content-Length'))
+        request.headers['Content-Length'] = old_content_length # type: ignore
+        #logger.debug("new request content length in core.py: %s", request.headers.get('Content-Length'))
         response.request = request
-        logger.debug("case in core.py: %s", case.body)
+        #logger.debug("case in core.py: %s", case.body)
         run_checks(
             case=case,
             checks=checks,
@@ -844,11 +837,12 @@ def _network_test(
         status = Status.failure
         raise
     finally:
-        # if feedback.stateful == Stateful.links:
-        # logger.debug("stateful")
+        if feedback.stateful == Stateful.links:
+            logger.debug("Stateful")
         # do something
 
         feedback.add_test_case(case, response)
+        # logger.debug("feedback in core.py: ", feedback)
         if store_interactions:
             # Lưu lại interaction
             result.store_requests_response(case, response, status, check_results)
